bc9be6b3a015b3c1e6ad15cee5429e8e3fd08699
leo virieu <leovirieu@gmail.com>
2016-10-29 15:47:33 +0200

    retouche style mini cart

--- left-column.php
+++ left-column.php
@@ -20,7 +20,7 @@
 							<div class="social-links">
 								<?php get_template_part( 'includes/social_icons', 'header' ); ?>
 							</div>
-							<div class="copyright"><span>2015-<?php echo date('Y'); ?> © <a href="www.clementribe.fr">C R</a href="www.leovirieu.fr"> & <a>L V</a></span></div>
+							<div class="copyright"><span>2015-<?php echo date('Y'); ?> © <a href="http://www.clementribe.fr" title="Clément Ribé">C R</a> & <a href="http://www.leovirieu.fr" title="Léo Virieu">L V</a></span></div>
 						</div>
 					</div>
 					<div class="middle-bar">
@@ -41,26 +41,63 @@
 								<?php show_page_menu( $menuClass, false, false ); ?>
 								<?php show_categories_menu( $menuClass, false ); ?>
 							</ul>
-							<?php if ( false !== et_get_option( 'show_search_icon', true ) || is_customize_preview() ) : ?>
-							<?php if ( 'fullscreen' !== et_get_option( 'header_style', 'left' ) ) { ?>
-								<div class="clear"></div>
-							<?php } ?>
-							<form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-								<?php
-								printf( '<input type="search" class="et-search-field" placeholder="%1$s" placeholder="%2$s" name="s" title="%3$s" />',
-									esc_attr__( 'Search &hellip;', 'Divi' ),
-									get_search_query(),
-									esc_attr__( 'Search for:', 'Divi' )
-								);
-								?>
-								<button type="submit" id="searchsubmit_header"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/search-icon.svg" /></button>
-							</form>
-						<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
 							<?php
 						else :
 							echo( $primaryNav );
 						endif;
 						?>
+						<!-- #contact -->
+						<div id="et-info">
+						<?php if ( '' !== ( $et_phone_number = et_get_option( 'phone_number' ) ) ) : ?>
+							<a href="<?php echo esc_attr( 'callto:' . $et_phone_number ); ?>"><span><?php echo esc_html( $et_phone_number ); ?></span></a>
+						<?php endif; ?>
+
+						<?php if ( '' !== ( $et_email = et_get_option( 'header_email' ) ) ) : ?>
+							<a href="<?php echo esc_attr( 'mailto:' . $et_email ); ?>"><span><?php echo esc_html( $et_email ); ?></span></a>
+						<?php endif; ?>
+
+						<?php
+						if ( true === $show_header_social_icons ) {
+							get_template_part( 'includes/social_icons', 'header' );
+						} ?>
+						</div> <!-- #et-info -->
+						<!-- #fin-contact -->
+						<!-- #search -->
+						<?php if ( false !== et_get_option( 'show_search_icon', true ) || is_customize_preview() ) : ?>
+						  <?php if ( 'fullscreen' !== et_get_option( 'header_style', 'left' ) ) { ?>
+						    <div class="clear"></div>
+						  <?php } ?>
+						  <form role="search" method="get" class="et-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+						    <?php
+						      printf( '<input type="search" class="et-search-field" placeholder="" placeholder="%2$s" name="s" title="%3$s" />',
+						        esc_attr__( 'Search &hellip;', 'Divi' ),
+						        get_search_query(),
+						        esc_attr__( 'Search for:', 'Divi' )
+						      );
+						    ?>
+						    <button type="submit" id="searchsubmit_header"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/search-icon.svg" /></button>
+						  </form>
+						<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
+						<!-- #end-search -->
+						<!-- #et-secondary-menu -->
+						<?php
+							if ( is_page_template( 'page-template-blank.php' ) ) {
+								return;
+							}
+
+							$et_secondary_nav_items = et_divi_get_top_nav_items();
+
+							$et_secondary_nav = $et_secondary_nav_items->secondary_nav;
+
+						?>
+						<div id="et-secondary-menu">
+						<?php
+							if ( '' !== $et_secondary_nav ) {
+								echo $et_secondary_nav;
+							}
+						?>
+						</div>
+						<!-- #end-et-secondary-menu -->
 					</div>
 				</nav>
 			<?php endif; ?>
@@ -73,6 +110,7 @@
 				<div id="et_top_search">
 					<span id="et_search_icon"></span>
 				</div>
+
 			<?php endif; // true === et_get_option( 'show_search_icon', false ) ?>
 
 			<?php do_action( 'et_header_top' ); ?>
