2f55e3079e2247f8be358e4592c2400d34aeb9e9
clement ribe <clement.ribe@gmail.com>
2016-11-13 15:42:08 +0100

    [add] Champs additionnels produit

---
+++ woocommerce/single-product/tabs/size-tab.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+global $product;
+
+$heightText = (get_post_meta($product->id, 'height_text', true) != '') ? get_post_meta($product->id, 'height_text', true) : 0;
+$widthText = (get_post_meta($product->id, 'width_text', true) != '') ? get_post_meta($product->id, 'width_text', true) : 0;
+$depthText = (get_post_meta($product->id, 'depth_text', true) != '') ? get_post_meta($product->id, 'depth_text', true) : 0;
+?>
+
+<div class="material-tab">
+    <h2><?php _e('Dimensions', 'ee') ?></h2>
+    <span><?php _e('Hauteur', 'ee') ?></span>
+    <?php echo $heightText ?>
+    <span><?php _e('Largeur', 'ee') ?></span>
+    <?php echo $widthText ?>
+    <span><?php _e('Profondeur', 'ee') ?></span>
+    <?php echo $depthText ?>
+</div>
